8ad2c6cd7e8157609b8bd479b22f70f97d899944
Max Lee <mail@moep.tv>
2023-05-13 14:27:20 +0100

    Fix name not getting colored when not listed
    Also allow more colors to be used and show color on set.

--- scripts/xchatnickcolor.pl
+++ scripts/xchatnickcolor.pl
@@ -1,7 +1,7 @@
 use strict;
 use Irssi 20020101.0250 ();
 use vars qw($VERSION %IRSSI); 
-$VERSION = "1.1";
+$VERSION = "1.1.2";
 %IRSSI = (
     authors     => "Timo Sirainen, Ian Peters, Max Lee",
     contact => "tss\@iki.fi, Max Lee: mail\@moep.tv", 
@@ -9,7 +9,7 @@ $VERSION = "1.1";
     description => "assign a different color for each nick",
     license => "Public Domain",
     url     => "http://irssi.org/",
-    changed => "2018-01-18T15:13+0100"
+    changed => "2023-05-13T14:18+0100"
 );
 
 my %saved_colors;
@@ -83,9 +83,10 @@ sub sig_public {
   my $chanrec = $server->channel_find($target);
   return if not $chanrec;
   my $nickrec = $chanrec->nick_find($nick);
-  return if not $nickrec;
-  my $nickmode = $nickrec->{op} ? "@" : $nickrec->{voice} ? "+" : "";
-
+  my $nickmode = "";
+  if ($nickrec) {
+    $nickmode = $nickrec->{op} ? "@" : $nickrec->{voice} ? "+" : "";
+  }
   # Has the user assigned this nick a color?
   my $color = $saved_colors{$nick};
 
@@ -126,10 +127,11 @@ sub cmd_color {
       Irssi::print ("Nick not given");
     } elsif (!$color) {
       Irssi::print ("Color not given");
-    } elsif ($color < 2 || $color > 14) {
-      Irssi::print ("Color must be between 2 and 14 inclusive");
+    } elsif ($color < 0 || $color > 15) {
+      Irssi::print ("Color must be between 0 and 15 inclusive");
     } else {
       $saved_colors{$nick} = $color;
+      Irssi::print ("Set color of $nick to ". chr (3) . "$color$color");
     }
   } elsif ($op eq "clear") {
     if (!$nick) {
